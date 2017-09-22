c5a5fc4197e9f2b26cc4e5830cec49924f25a38b
Icaro Alcantara Laudade <icaroalcantara11@hotmail.com>
2017-09-21 21:47:18 -0300

    Problema 1198-O Bravo Guerreiro Hashmat-C#

---
+++ 1198/1198/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1198
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+
+            while ((line = System.Console.ReadLine()) != null)
+            {
+                string[] linha = line.Split(' ');
+                long a = long.Parse(linha[0]);
+                long b = long.Parse(linha[1]);
+                long c = a - b;
+                if (c > 0)
+                {
+                    Console.WriteLine(c);
+                }
+                else {
+                    Console.WriteLine(c * -1);
+                }
+            }
+        }
+    }
+}
